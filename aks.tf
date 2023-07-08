d51366f9ffc71eb6e177dc08beeaa102296e26ca
andaldev <aeacoder@gmail.com>
2023-07-08 23:55:52 +0200

    enabled azure rbac

--- aks.tf
+++ aks.tf
@@ -1,16 +1,13 @@
 resource "azurerm_kubernetes_cluster" "juno" {
-  name                          = "${var.project}-${var.env}-aks"
-  location                      = azurerm_resource_group.juno.location
-  resource_group_name           = azurerm_resource_group.juno.name
-  dns_prefix                    = var.prefix
-  kubernetes_version            = var.kubernetes_version
-  tags                          = local.tags
-  sku_tier                      = var.sku_tier
-  oidc_issuer_enabled           = true
-  workload_identity_enabled     = true
-  local_account_disabled        = true
-  enable_azure_active_directory = true
-  rbac_aad_managed              = true
+  name                      = "${var.project}-${var.env}-aks"
+  location                  = azurerm_resource_group.juno.location
+  resource_group_name       = azurerm_resource_group.juno.name
+  dns_prefix                = var.prefix
+  kubernetes_version        = var.kubernetes_version
+  tags                      = local.tags
+  sku_tier                  = var.sku_tier
+  oidc_issuer_enabled       = true
+  workload_identity_enabled = true
 
   default_node_pool {
     name                = "juno"
@@ -22,6 +19,11 @@ resource "azurerm_kubernetes_cluster" "juno" {
 
   identity {
     type = "SystemAssigned"
+  }
+
+  azure_active_directory_role_based_access_control {
+    managed            = true
+    azure_rbac_enabled = true
   }
 }
 
